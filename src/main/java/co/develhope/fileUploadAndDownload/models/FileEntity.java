98969161f37c58e52a2fb0eab14b0f69eee2af05
Calyen93 <genco.claudio3@gmail.com>
2024-06-21 11:23:55 +0200

    Fix problem on GenerationType

--- src/main/java/co/develhope/fileUploadAndDownload/models/FileEntity.java
+++ src/main/java/co/develhope/fileUploadAndDownload/models/FileEntity.java
@@ -1,9 +1,6 @@
 package co.develhope.fileUploadAndDownload.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
+import jakarta.persistence.*;
 
 @Entity
 public class FileEntity {
